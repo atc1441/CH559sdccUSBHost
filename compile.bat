6b132f0543bd4027b3c65af046157bcb7704fbeb
atc1441 <1102694+atc1441@users.noreply.github.com>
2020-03-26 09:43:28 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -30,5 +30,5 @@ del *.rst
 del *.sym
 
 
-// This tool flashes the bin file directly to the ch559 chip, you need to install the libusb-win32 driver with the zadig( https://zadig.akeo.ie/ ) tool so the tool can access the usb device
+Rem This tool flashes the bin file directly to the ch559 chip, you need to install the libusb-win32 driver with the zadig( https://zadig.akeo.ie/ ) tool so the tool can access the usb device
 chflasher.exe %project_name%.bin
